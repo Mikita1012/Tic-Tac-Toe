8833c892af30a19cc3352e238577804504ca0c22
Mikita1012 <mikitashah2009@gmail.com>
2024-05-13 15:41:50 +0530

    Layout of board

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css'
+
+export default function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className='board-row'>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className='board-row'>
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+
+    </>
+  )
+}
