713554c5d332e58c1830bdd836bb777f46e2fcbc
Mikita1012 <mikitashah2009@gmail.com>
2024-05-13 16:37:22 +0530

    Added array having nine null values initially for 9 squares in board and passed as PROPS in SQ

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import './App.css'
 import { useState } from 'react';
 
-function Sqaure() { //child component
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue('X');
-  }
-  return <button className='square' onClick={handleClick}>{value}</button>
+function Sqaure({value}) { //child component
+  // const [value, setValue] = useState(null);
+  // function handleClick() {
+  //   setValue('X');
+  // }
+  return <button className='square'>{value}</button>
 }
 
 export default function Board() { //parent component
+  const[squares, setSquares] = useState(Array(9).fill(null))
   return (
     <>
       <div className='board-row'>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[0]} />
+        <Sqaure value={squares[1]} />
+        <Sqaure value={squares[2]} />
       </div>
       <div className='board-row'>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[3]} />
+        <Sqaure value={squares[4]} />
+        <Sqaure value={squares[5]} />
       </div>
       <div className='board-row'>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[6]} />
+        <Sqaure value={squares[7]} />
+        <Sqaure value={squares[8]} />
       </div>
 
     </>
